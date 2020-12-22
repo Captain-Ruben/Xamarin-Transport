bd1a09e70c9a1a0e07e1455c25b5423b09ee0a03
Ruben Lucas <ruben.lucas.p@gmail.com>
2020-12-22 19:39:18 +0100

    Create

---
+++ Transport/Transport/Transport/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Transport.ViewModels;
+using Xamarin.Forms;
+
+namespace Transport.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
